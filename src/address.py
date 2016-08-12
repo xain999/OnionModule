0c3ac5c394c90ad93fe8c423c256ac7274ac9a65
Zain Ul Abideen <zain.ul.abideen@tum.de>
2016-08-13 00:24:06 +0200

    recvAll() function added

--- src/address.py
+++ src/address.py
@@ -1,5 +1,19 @@
+# system imports
+import socket
+
 class Address(object):
     def __init__(self, ip, port, ipv6):
         self.ip = ip
         self.port = port
         self.ipv6 = ipv6
+
+
+def receiveAll(sock, size):
+    data = ''
+
+    while size > 0:
+        recvd = sock.recv(size)
+        size = size - len(recvd)
+        data = data + recvd
+
+    return data
